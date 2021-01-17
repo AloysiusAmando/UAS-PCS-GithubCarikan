14f46a7e6a592c1bac46b572fad2da998e11923b
AloysiusAmando <aloysius.amando@students.amikom.ac.id>
2021-01-17 17:40:20 +0700

    first commit

--- app/src/main/java/com/app/githubcarikan/api/Api.kt
+++ app/src/main/java/com/app/githubcarikan/api/Api.kt
@@ -11,25 +11,25 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 914fba1154be17e8d11e4feaef615734fb9e74a6")
+    @Headers("Authorization: token 687a155f2679f01c4c19dafc5011ca87c96c41c7")
     fun getSearchUsers(
         @Query("q") query: String
     ) : Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers ("Authorization: token 914fba1154be17e8d11e4feaef615734fb9e74a6")
+    @Headers ("Authorization: token 687a155f2679f01c4c19dafc5011ca87c96c41c7")
     fun getUserDetail(
         @Path("username") username: String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 914fba1154be17e8d11e4feaef615734fb9e74a6")
+    @Headers("Authorization: token 687a155f2679f01c4c19dafc5011ca87c96c41c7")
     fun getFollowers(
         @Path("username") username: String
     ): Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 914fba1154be17e8d11e4feaef615734fb9e74a6")
+    @Headers("Authorization: token 687a155f2679f01c4c19dafc5011ca87c96c41c7")
     fun getFollowing(
         @Path("username") username: String
     ): Call<ArrayList<User>>
